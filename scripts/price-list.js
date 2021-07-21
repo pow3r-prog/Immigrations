1452bf99e47404ecf50e03824635b96e30b98669
Vladimir <vova.andreythuk40@gmail.com>
2021-07-21 19:01:11 +0300

    updating price block & adding functionality

--- scripts/price-list.js
+++ scripts/price-list.js
@@ -16,13 +16,32 @@ jQuery(($) => {
 		$(this).parent().fadeOut();
 		$(this).parent().prev().text($(this).text());
 		$(this).parent().prev().prev().val($(this).text());
+		if ($(this).text() == "Vip на 30дней - 50грн.") {
+			$('.price_vip').text("50");
+		}
+		if ($(this).text() == "Vip на 60дней - 90грн.") {
+			$('.price_vip').text("90");
+		}
+		if ($(this).text() == "Vip на 120дней - 160грн.") {
+			$('.price_vip').text("160");
+		}
 	});
+
 
 	$('.select').on('click', '.select__item_admin', function () {
 		$('.select__head').removeClass('open');
 		$(this).parent().fadeOut();
 		$(this).parent().prev().text($(this).text());
 		$(this).parent().prev().prev().val($(this).text());
+		if ($(this).text() == "Admin на 30дней - 100грн.") {
+			$('.price_admin').text("100");
+		}
+		if ($(this).text() == "Admin на 60дней - 180грн.") {
+			$('.price_admin').text("180");
+		}
+		if ($(this).text() == "Admin на 120дней - 320грн.") {
+			$('.price_admin').text("320");
+		}
 	});
 
 	$(document).click(function (e) {
@@ -31,4 +50,5 @@ jQuery(($) => {
 			$('.select__list').fadeOut();
 		}
 	});
+
 });
